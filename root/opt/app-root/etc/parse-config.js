6683e1aa8e06e54dffc2cb8c41676db5068a3739
root <rcdelacruz@gmail.com>
2020-08-11 09:32:33 +0800

    Added parse-config.js

---
+++ root/opt/app-root/etc/parse-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
+    appId: process.env.APP_ID || 'myAppId',
+    clientKey: process.env.CLIENT_KEY || 'myClientKey',
+    masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+    javascriptKey: process.env.JAVASCRIPT_KEY || 'myJSKey',
+    restAPIKey: process.env.REST_API_KEY || 'restAPIKey',
+    cloud: process.env.PARSE_CLOUD_CODE || './cloud/main.js',
+    liveQuery: {
+      classNames: process.env.LIVE_QUERY_CLASSNAMES
+        ? process.env.LIVE_QUERY_CLASSNAMES.split(',')
+        : [],
+    },
+    push: (process.env.PUSH_ENABLED === "true") ? {
+      android: {
+        apiKey: process.env.ANDROID_API_KEY,
+      },
+      ios: {
+        pfx: process.env.IOS_CERT_PATH,
+        passphrase: process.env.IOS_PASSPHRASE || '',
+        bundleId: process.env.IO_BUNDLE_ID,
+        production: process.env.IOS_PRODUCTION_MODE || false,
+      }
+    } : {},
+    verbose: false
+  }
+  
